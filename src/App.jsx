961b6fbbecd120222941b442150c9c44033431b0
Deepak Sharma <83909821+deepak08cdm@users.noreply.github.com>
2022-03-13 15:49:40 +0000

    commit happend

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const handleClick = () =>{
+    console.log('hello')
+  }
   return (
     <main>
       React⚛️ + Vite⚡ + Replit🌀
+      <button onClick={handleClick}>click me</button>
     </main>
   );
 }
