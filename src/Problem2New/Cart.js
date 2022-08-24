f0e67452eccba9d617fd067959acd152bb7238ba
Salil Patel <101462890+salilpatel@users.noreply.github.com>
2022-08-24 14:06:11 +0000

    problem2 styling

--- src/Problem2New/Cart.js
+++ src/Problem2New/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "./Style/Cart.css"
 
 const Cart = ({ cart, setCart, setCartLength }) => {
   const handleRemove = (id) => {
@@ -8,24 +9,28 @@ const Cart = ({ cart, setCart, setCartLength }) => {
   }
 
   return (
-    <article>
+    <div className='container'>
       <h2 className="text-center">You have Ordered:</h2>
-      {cart.map((item) => (
-        <div key={item.id} className='row'>
-          <div className='col-sm-2'>
-            <img style={{ width: '150px' }} src={item.img} alt={''} />
+      <div id='cartItems'>
+        {cart.map((item) => (
+          <div id={'card'}>
+            <div key={item.id} className={'row'} id={'innerCard'}>
+              <div className={'col-sm-2'} id={'imgContainer'}>
+                <img style={{ width: '100px' }} id={'image'} src={item.img} alt={''} />
+              </div>
+              <div className={'col-sm-7'} style={{ paddingLeft: '20px' }}>
+                <h3>{item.shirtType}</h3>
+                <p>{item.description}</p>
+                <h4>{item.actualPrice}</h4>
+              </div>
+              <div className={'col-sm-2'} style={{ marginTop: '40px' }}>
+                <button className={'btn btn-danger'} onClick={() => handleRemove(item.id)}>REMOVE</button>
+              </div>
+            </div>
           </div>
-          <div className='col-sm-8'>
-            <h3>{item.shirtType}</h3>
-            <p>{item.description}</p>
-            <h4>{item.actualPrice} <del>{item.fakePrice}</del></h4>
-          </div>
-          <div className='col-sm-2' style={{ marginTop: '40px' }}>
-            <button onClick={() => handleRemove(item.id)}>Remove</button>
-          </div>
-        </div>
-      ))}
-    </article>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/Problem2New/Home.js
+++ src/Problem2New/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
 import products from "./Product";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "./Style/Home.css"
 
 const Home = ({ handleClick }) => {
 
   return (
     <div className="container">
       <h1 className="text-center">Our items</h1>
-      <div className="row">
+      <div className="row cartItems">
         {products.map((item) => (
-          <div key={item.id} className="col-sm">
-            <div>
-              <img style={{ width: '350px' }} src={item.img} />
+          <div key={item.id} className={"col-lg card"}>
+            <div className={'imgContainer'}>
+              <img className={'image'} src={item.img} />
             </div>
-            <div style={{ paddingLeft: '30px' }}>
+            <div className={'cardBody'}>
               <h2>{item.shirtType}</h2>
               <p>{item.description}</p>
               <h3>{item.actualPrice} <del>{item.fakePrice}</del></h3>
